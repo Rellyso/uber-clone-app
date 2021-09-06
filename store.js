5a5de76fb75925dab6e7f0f8111f6d72aafb4a00
Rellyson Douglas <rellysondouglas2015@gmail.com>
2021-09-06 07:12:52 -0300

    configured redux

---
+++ store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import navReducer from './src/slices/navSlice'
+
+export const store = configureStore({
+  reducer: {
+    nav: navReducer
+  }
+})
+// The store now has redux-thunk added and the Redux DevTools Extension is turned on
