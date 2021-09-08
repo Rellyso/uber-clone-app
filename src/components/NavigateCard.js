a537d0626d653b312645139c3ddac8b70e46efb3
Rellyson Douglas <rellysondouglas2015@gmail.com>
2021-09-08 07:42:35 -0300

    created search destination component

---
+++ src/components/NavigateCard.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Text, SafeAreaView, View, StyleSheet } from 'react-native'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import tw from 'tailwind-react-native-classnames'
+import { GOOGLE_MAPS_API_KEY } from '@env'
+import { useDispatch } from 'react-redux'
+import { useNavigation } from '@react-navigation/native'
+import { setDestination } from '../slices/navSlice'
+
+export const NavigateCard = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+
+  return (
+    <SafeAreaView
+      style={tw`bg-white flex-1`}
+    >
+      <Text
+        style={tw`text-center py-5 text-xl`}
+      >
+        Good morning, Someone
+      </Text>
+
+      <View style={tw`border-t border-gray-200 flex-shrink`}>
+        <View style={tw`px-5`}>
+          <GooglePlacesAutocomplete
+            placeholder="Where to?"
+            styles={toInputBoxStyles}
+
+            returnKeyType={"search"}
+            minLength={2}
+
+            fetchDetails={true}
+            enablePoweredByContainer={false}
+            nearbyPlacesAPI="GooglePlacesSearch"
+            debounce={400}
+
+            onPress={(data, details = null) => {
+              dispatch(
+                setDestination({
+                  location: details.geometry.location,
+                  description: data.description,
+                }));
+
+              navigation.navigate('RideOptions')
+            }}
+
+            query={{
+              key: GOOGLE_MAPS_API_KEY,
+              language: 'pt-BR'
+            }}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+const toInputBoxStyles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    paddingTop: 20,
+    flex: 0,
+  },
+  textInput: {
+    backgroundColor: '#f2f2f2'
+  }
+})
